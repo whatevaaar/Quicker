f4d65679a6a916be47ae61523ec16c6c67fbefc7
Whatevaaar <emilianohidalgo314@gmail.com>
2020-10-16 17:02:25 -0500

    pdf

--- build/js/generador.js
+++ build/js/generador.js
@@ -1,23 +1,27 @@
 const tablaModal = document.getElementById("tabla-modal");
-const tablaModalPadre = document.getElementById("tabla-modal-padre");
 const parrafoTotal = document.getElementById("p-total");
 
+const bodyTablaPDF = [
+    ['Estrategia', 'Táctica', 'Costo', 'Cantidad', 'Total']
+];
 
 function generarPropuesta() {
     tablaModal.textContent = '';
     $('[name="input-cantidad-tacticas"]').each(function () {
         let input = $(this);
-        if (input && input.val() > 0)
+        if (input && input.val() > 0) {
             tablaModal.appendChild(crearHilera(input));
+            bodyTablaPDF.push([input.attr('data-estrategia'), input.attr('data-nombre'), input.attr('data-costo'), input.val(), input.val() * input.attr('data-costo')]);
+        }
     });
-    actualizarTextoTotal();
     $('#myModal').modal(show = true);
+    actualizarTextoTotal();
 }
 
 function actualizarTextoTotal() {
     let suma = 0;
-    $('[name="columna-total"]').each(function () {
-        suma += $(this).innerText();
+    $('.columna-total').each(function () {
+        suma += Number($(this).html());
     });
     parrafoTotal.innerText = 'Total $' + suma;
 }
@@ -28,7 +32,7 @@ function crearCeldaEliminar(propuesta) {
     let icono = document.createElement('i');
     enlace.href = "#";
     enlace.onclick = function () {
-        eliminarDeCarrito(idProducto);
+        tablaModal.removeChild(document.getElementById(propuesta));
     }
     icono.classList.add("far");
     icono.classList.add("fa-window-close");
@@ -44,14 +48,14 @@ function crearHilera(propuesta) {
     let columnaCantidad = document.createElement('td');
     let columnaCosto = document.createElement('td');
     let columnaTotal = document.createElement('td');
-    let columnaEliminar = crearCeldaEliminar(propuesta);
+    let columnaEliminar = crearCeldaEliminar(propuesta.attr('data-nombre'));
 
     columnaEstrategia.innerText = propuesta.attr('data-estrategia');
     columnaTactica.innerText = propuesta.attr('data-nombre');
     columnaCosto.innerText = propuesta.attr('data-costo');
     columnaCantidad.innerText = propuesta.val();
     columnaTotal.innerText = propuesta.val() * propuesta.attr('data-costo');
-    columnaTotal.name = 'columna-total';
+    columnaTotal.classList.add('columna-total');
 
     hilera.appendChild(columnaEstrategia);
     hilera.appendChild(columnaTactica);
@@ -66,21 +70,25 @@ function crearHilera(propuesta) {
 }
 
 function generar() {
-
-    pdfMake.createPdf(createTableDoc()).open();
+    crearPdf();
 }
 
-function createTableDoc() {
-    var wdths= [];
-    for (var i = 0; i < tablaModalPadre.rows[0].cells.length; i++) {
-        var cell = tablaModalPadre.rows[0].cells[i];
-        wdths[i] = (cell.style.width != "" ? cell.style.width : cell.style.offsetWidth); //if the cell's style width is not set, get its' actual width
-    }
+function crearPdf() {
+    var docDefinition = {
+        content: [
+            {
+                layout: 'lightHorizontalLines', // optional
+                table: {
+                    // headers are automatically repeated if the table spans over multiple pages
+                    // you can declare how many rows should be treated as headers
+                    headerRows: 1,
+                    widths: ['auto', 'auto', 'auto', 'auto', 'auto'],
 
-    var bdy;
-    for (var y = 0; y < tablaModalPadre.rows.length; y++)
-        for (var x = 0; x < tablaModalPadre.rows[y].cells.length; x++)
-            bdy[y][x] = tablaModalPadre.rows[y].cells[x].innerHTML;
-    var docDef = {content: [{table: {headerRows: 1, widths: wdths, body: bdy}}]};
-    return docDef;
+                    body: bodyTablaPDF.slice()
+                }
+            }
+        ]
+    };
+
+    pdfMake.createPdf(docDefinition).download();
 }
